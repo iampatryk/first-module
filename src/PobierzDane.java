30da4fa9c80daeebeec6d9b8c1132948ced5e4d1
Patryk <84429106+xc3mill@users.noreply.github.com>
2024-02-11 16:56:30 +0100

    new update

---
+++ src/PobierzDane.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PobierzDane {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jak masz na imię?");
+        String imie = scanner.nextLine();
+
+        System.out.println("Jak masz na nazwisko?");
+        String nazwisko = scanner.nextLine();
+
+        System.out.println("Ile masz lat?");
+        int wiek = scanner.nextInt();
+
+        System.out.println("Imię: " + imie + "\n" + "Nazwisko: " + nazwisko + "\n" + "Wiek: " + wiek);
+    }
+}
