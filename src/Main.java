5d036ab123207a8944378dc132d6d7e51d3bc61d
Patryk <84429106+xc3mill@users.noreply.github.com>
2024-02-11 15:49:37 +0100

    new update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(" i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Jak masz na imię ? ");
+        String imie = scanner.nextLine();
+
+        System.out.println("Ile masz lat?");
+        String wiek = scanner.nextLine();
+
+        System.out.println("Cześć " + imie + "! " + "Masz " + wiek + " lat!");
+
     }
 }
