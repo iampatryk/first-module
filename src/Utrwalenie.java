6dfa49b71b1ae34abf2c50db0bfacdf975827846
Patryk <84429106+xc3mill@users.noreply.github.com>
2024-02-12 14:27:48 +0100

    new update

---
+++ src/Utrwalenie.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Utrwalenie {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj ile zarabiasz na godzine: ");
+        double stawka = scanner.nextDouble();
+
+        System.out.println("Zarabiasz rocznie: " + (stawka * 2080) + " złotych");
+        System.out.println("Zarabiasz miesięcznie: " + (stawka * 160) + " złotych");
+
+    }
+}
